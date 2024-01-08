0566cb484d94359f26556f46bb18c9326b28d1eb
IvanSMMA <155035815+IvanSMMA@users.noreply.github.com>
2024-01-08 16:18:53 -0600

    Update script.js

--- nuevoingreso/script.js
+++ nuevoingreso/script.js
@@ -19,3 +19,38 @@ change1.addEventListener('click', () => {
 change2.addEventListener('click', () => {
     container.classList.remove("active");
 });
+
+
+const signInForm = document.getElementById('signInForm');
+const signInBtn = document.getElementById('signInBtn');
+
+signInBtn.addEventListener('click', function(event) {
+
+const email = signInForm.querySelector('#mailin').value;
+const password = signInForm.querySelector('#contrain').value;
+
+
+if (!email || !password) {
+    alert('Please complete the text fields ');
+    event.preventDefault(); 
+} else {
+    alert('Welcome again');
+}
+});
+
+const signupForm = document.getElementById('signupForm');
+const signupBtn = document.getElementById('signupBtn');
+
+signupBtn.addEventListener('click', function(event) {
+
+const emailup = signupForm.querySelector('#mail').value;
+const passwordup = signupForm.querySelector('#contra').value;
+
+
+if (!emailup || !passwordup) {
+    alert('Please complete the text fields ');
+    event.preventDefault(); 
+} else {
+    alert('Account successfully registered');
+}
+});
